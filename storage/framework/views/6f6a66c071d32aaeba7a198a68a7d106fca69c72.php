1ebc6866ffc45a98d5fb2d51051c3fd39b5ff6dc
rictu6 <110890728+rictu6@users.noreply.github.com>
2023-08-04 15:06:46 +0800

    Delete storage directory

--- storage/framework/views/6f6a66c071d32aaeba7a198a68a7d106fca69c72.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer text-center">
-    <?php echo $__env->make('partials.socials', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <strong><?php echo e($info['footer']); ?></strong>
- 
-</footer><?php /**PATH C:\laragon\www\Interweb\resources\views/partials/footer.blade.php ENDPATH**/ ?>
